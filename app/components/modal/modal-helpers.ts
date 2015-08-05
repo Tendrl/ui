b9d86934ba13e1f4e2178b548f2c98d776dd5281
Kanagaraj M <kmayilsa@redhat.com>
2015-08-05 11:37:02 +0530

    Added Volume and Modal helper functions
    Added VolumeHelpers and ModalHelpers
    
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

---
+++ app/components/modal/modal-helpers.ts
@@ -0,0 +1,47 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+export function UnAuthorized($modal, options) {
+    options = options || {};
+    return $modal(_.extend({
+        title: 'UNAUTHORIZED ACCESS',
+        content: 'Unauthorized access to API. It looks like your authentication tokens are invalid. Please try logging out and back in again.',
+        container: 'body',
+        template: 'views/custom-modal.html'
+    }, options));
+}
+
+export function UnexpectedError($modal, options) {
+    options = options || {
+        status: '?'
+    };
+    return $modal(_.extend({
+        title: 'UNEXPECTED ERROR (' + options.status + ')',
+        container: 'body',
+        template: 'views/custom-modal.html'
+    }, options));
+}
+
+export function SuccessfulRequest($modal, options) {
+    options = options || {};
+    return $modal(_.extend({
+        content: 'This may take a while. We\'ll let you know when it\'s done.',
+        container: 'body',
+        template: 'views/custom-modal.html',
+        backdrop: 'static', // disable mouse clicks for now since I can't wrap them or supply a callback
+        keyboard: false
+    }, options));
+}
+export function makeOnError(modal, callback) {
+    return function onError(resp) {
+        modal.$scope.title = 'Unexpected Error ' + resp.status;
+        modal.$scope.content = '<pre>' + resp.data + '</pre>';
+        modal.$scope.$hide = _.wrap(modal.$scope.$hide, function($hide) {
+            $hide();
+            if (_.isFunction(callback)) {
+                callback.call(undefined);
+            }
+        });
+        modal.$scope.disableClose = false;
+        modal.$scope.$show();
+    };
+}
