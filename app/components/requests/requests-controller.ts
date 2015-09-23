2a73fd036b6674e59170be980e1dd00c1a50b48f
yazhini <yazhiniloganathan1994@gmail.com>
2015-09-23 15:23:39 +0530

    request controller is added

--- app/components/requests/requests-controller.ts
+++ app/components/requests/requests-controller.ts
@@ -6,8 +6,14 @@ var idbWrapper = require('idb-wrapper');
 import {UserService} from '../rest/user';
 import {ServerService} from '../rest/server';
 import {RequestTrackingService} from './request-tracking-svc';
+import {UtilService} from '../rest/util';
+import {RequestService} from '../rest/request';
 
 export class RequestsController {
+   private tasks;
+   private alerts : Array<any>;
+   private discoveredHosts : Array<any>;
+   private discoveredHostsLength: number;
     static $inject: Array<string> = [
         '$scope',
         '$interval',
@@ -18,128 +24,127 @@ export class RequestsController {
         'RequestService',
         'RequestTrackingService',
         'UserService'];
-    constructor(
-        $scope: any,
-        $interval: ng.IIntervalService,
-        $timeout: ng.ITimeoutService,
-        $log: ng.ILogService,
-        serverSvc: ServerService,
-        utilSvc: any,
-        requestSvc: any,
-        requestTrackingService: RequestTrackingService,
-        userSvc: UserService) {
 
-        $scope.alerts = [];
-        $scope.tasks = [];
-        $scope.discoveredHostsLength = 0;
-        $scope.discoveredHosts = [];
+    constructor(private $scope: any,
+        private $interval: ng.IIntervalService,
+        private $timeout: ng.ITimeoutService,
+        private $log: ng.ILogService,
+        private serverSvc: ServerService,
+        private utilSvc: UtilService,
+        private requestSvc: RequestService,
+        private requestTrackingService: RequestTrackingService,
+        private userSvc: UserService) {
+        this.alerts = [];
+        this.tasks = {};
+        this.discoveredHostsLength = 0;
+        this.discoveredHosts = [];
+        this.$interval(() => this.reloadDiscoveredHosts(), 5000);
+        this.$interval(() => this.reloadAlerts(), 6000);
+        this.$interval(() => this.reloadTasks(), 5000);
+    }
 
-        $scope.reloadAlerts = function() {
-            serverSvc.getList().then(function(hosts) {
-                var alerts = [];
-                _.each(hosts, function(host: any) {
-                    if (host.node_status === 1) {
-                        alerts.push('Host ' + host.node_name + ' is down');
-                    }
-                });
-                $scope.alerts = alerts;
-            });
-        }
-
-        $scope.reloadTasks = function() {
-            requestTrackingService.getTrackedRequests().then(function(tasks) {
-                $scope.tasks = tasks;
-            });
-        }
-
-
-        $scope.logoutUser = function() {
-            userSvc.logout().then(function(logout) {
-                document.location.href = '';
-            });
-        }
-
-        $scope.reloadDiscoveredHosts = function() {
-
-            $scope.discoveredHosts = _.filter($scope.discoveredHosts, function(host: any) {
-                return host.state !== "ACCEPTED" && host.state !== "UNACCEPTED";
-            });
-
-            serverSvc.getDiscoveredHosts().then(function(freeHosts) {
-
-                $scope.discoveredHostsLength = freeHosts.length;
-
-                _.each(freeHosts, function(freeHost: any) {
-                    var host = {
-                        hostname: freeHost.node_name,
-                        ipaddress: freeHost.management_ip,
-                        state: "UNACCEPTED",
-                        selected: false
-                    };
-
-                    var isPresent = false;
-
-                    isPresent = _.some($scope.discoveredHosts, function(dHost: any) {
-                        return dHost.hostname === host.hostname;
-                    });
-
-                    if (!isPresent) {
-                        $scope.discoveredHosts.push(host);
-                    }
-                });
-            });
-        }
-
-        $scope.acceptHost = function(host) {
-            var hosts = {
-                nodes: [
-                    {
-                        node_name: host.hostname,
-                        management_ip: host.ipaddress
-                    }
-                ]
-            };
-
-            utilSvc.acceptHosts(hosts).then(function(result) {
-                $log.info(result);
-                host.state = "ACCEPTING";
-                host.task = result;
-                var callback = function() {
-                    requestSvc.get(result).then(function(request) {
-                        if (request.status === 'FAILED' || request.status === 'FAILURE') {
-                            $log.info('Failed to accept host in requests controller' + host.hostname);
-                            host.state = "FAILED";
-                            host.task = undefined;
-                        }
-                        else if (request.status === 'SUCCESS') {
-                            $log.info('Accepted host in requests controller ' + host.hostname);
-                            host.state = "ACCEPTED";
-                            host.task = undefined;
-                        }
-                        else {
-                            $log.info('Accepting host in requests controller' + host.hostname);
-                            $timeout(callback, 5000);
-                        }
-                    });
-                }
-                $timeout(callback, 5000);
-            });
-        };
-
-        $scope.openDiscoveredHostsModel = function() {
-            document.getElementById("openDiscoveredHosts").click();
-        }
-
-        $scope.acceptAllHosts = function() {
-            _.each($scope.discoveredHosts, function(host: any) {
-                if (host.state === "UNACCEPTED") {
-                    $scope.acceptHost(host);
+    public reloadAlerts() {
+        this.serverSvc.getList().then((hosts) => {
+            var alerts = [];
+            _.each(hosts, (host: any) => {
+                if (host.node_status === 1) {
+                    alerts.push('Host ' + host.node_name + ' is down');
                 }
             });
-        };
-         $interval($scope.reloadDiscoveredHosts, 5000);
-         $interval($scope.reloadAlerts, 6000);
-         $interval($scope.reloadTasks, 5000);
+            this.alerts = alerts;
+        });
+    }
 
+    public reloadTasks() {
+        this.requestTrackingService.getTrackedRequests().then((tasks) => {
+            this.tasks = tasks;
+        });
+    }
+
+
+    public logoutUser() {
+        this.userSvc.logout().then((logout) => {
+            document.location.href = '';
+        });
+    }
+
+    public reloadDiscoveredHosts() {
+
+        this.discoveredHosts = _.filter(this.discoveredHosts, (host: any) => {
+            return host.state !== "ACCEPTED" && host.state !== "UNACCEPTED";
+        });
+
+        this.serverSvc.getDiscoveredHosts().then((freeHosts) => {
+
+            this.discoveredHostsLength = freeHosts.length;
+
+            _.each(freeHosts, (freeHost: any) => {
+                var host = {
+                    hostname: freeHost.node_name,
+                    ipaddress: freeHost.management_ip,
+                    state: "UNACCEPTED",
+                    selected: false
+                };
+
+                var isPresent = false;
+
+                isPresent = _.some(this.discoveredHosts, (dHost: any) => {
+                    return dHost.hostname === host.hostname;
+                });
+
+                if (!isPresent) {
+                    this.discoveredHosts.push(host);
+                }
+            });
+        });
+    }
+
+    public acceptHost(host) {
+        var hosts = {
+            nodes: [
+                {
+                    node_name: host.hostname,
+                    management_ip: host.ipaddress
+                }
+            ]
+        }
+
+        this.utilSvc.acceptHosts(hosts).then((result) => {
+            this.$log.info(result);
+            host.state = "ACCEPTING";
+            host.task = result;
+            var callback = () => {
+                this.requestSvc.get(result).then((request) => {
+                    if (request.status === 'FAILED' || request.status === 'FAILURE') {
+                        this.$log.info('Failed to accept host in requests controller' + host.hostname);
+                        host.state = "FAILED";
+                        host.task = undefined;
+                    }
+                    else if (request.status === 'SUCCESS') {
+                        this.$log.info('Accepted host in requests controller ' + host.hostname);
+                        host.state = "ACCEPTED";
+                        host.task = undefined;
+                    }
+                    else {
+                        this.$log.info('Accepting host in requests controller' + host.hostname);
+                        this.$timeout(callback, 5000);
+                    }
+                });
+            }
+            this.$timeout(callback, 5000);
+        });
+    }
+
+    public openDiscoveredHostsModel() {
+        document.getElementById("openDiscoveredHosts").click();
+    }
+
+    public acceptAllHosts() {
+        _.each(this.discoveredHosts, (host: any) => {
+            if (host.state === "UNACCEPTED") {
+                this.acceptHost(host);
+            }
+        });
     }
 }
+    
