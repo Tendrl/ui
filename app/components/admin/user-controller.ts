846d4216e23c891001ee4142688be46b156f0fa3
Karnan TC <kchidamb@redhat.com>
2015-10-14 14:35:08 +0530

    added User management, userlisting page

---
+++ app/components/admin/user-controller.ts
@@ -0,0 +1,23 @@
+import {UserService} from '../rest/user';
+
+export class UserController {
+    private errorMsg;
+    private userList : Array<any>;
+    static $inject: Array<string> = [
+        '$location',
+        'UserService',
+    ];
+
+    constructor(
+        private $location: ng.ILocationService,
+        private UserService: UserService) {
+        this.getUsers();
+    }
+   
+    public getUsers(){
+        this.UserService.getUsers().then((users)=>{
+            this.userList = users;
+        });
+    }
+           
+}
