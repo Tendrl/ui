e08d283e68c7d33b40785b5cb7a9cbf727d3be6a
Kanagaraj M <kmayilsa@redhat.com>
2015-11-09 15:25:03 +0530

    Added Storage REST service
    which provides
    - getList
    - getListByCluster
    - get
    - create
    
    Signed-off-by: Kanagaraj M <kmayilsa@redhat.com>

---
+++ app/components/rest/storage.ts
@@ -0,0 +1,48 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+export class StorageService {
+    rest: restangular.IService;
+    restFull: restangular.IService;
+    static $inject: Array<string> = ['Restangular'];
+    constructor(rest: restangular.ICollection) {
+        this.rest = rest.withConfig((RestangularConfigurer) => {
+            RestangularConfigurer.setBaseUrl('/api/v1/');
+        });
+        this.restFull = rest.withConfig((RestangularConfigurer) => {
+            RestangularConfigurer.setBaseUrl('/api/v1/');
+            RestangularConfigurer.setFullResponse(true);
+        });
+    }
+
+
+    // **getList**
+    // **@returns** a promise with all storages.
+    getList(clusterId) {
+        return this.rest.one('clusters', clusterId).all('storages').getList().then(function(storages) {
+            return storages;
+        });
+    }
+
+    // **getListByCluster**
+    // **@returns** a promise  with all storages of the cluster.
+    getListByCluster(clusterId) {
+        return this.rest.one('clusters', clusterId).all('storages').getList().then(function(storages) {
+            return storages;
+        });
+    }
+
+    // **get**
+    // **@returns** a promise with storage metadata.
+    get(clusterId, storageId) {
+        return this.rest.one('clusters', clusterId).one('storages', storageId).get().then(function(storage) {
+            return storage;
+        });
+    }
+
+    // **create**
+    // **@param** storage - Information about the storage.
+    // **@returns** a promise which returns a request id to track the task.
+    create(clusterId, storage) {
+        return this.restFull.one('clusters', clusterId).all('storages').post(storage);
+    }
+}
