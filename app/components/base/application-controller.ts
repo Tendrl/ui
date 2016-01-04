99243d1c59b181a209535cde9b01c219824aa1dc
Karnan <kchidamb@redhat.com>
2016-01-04 15:58:13 +0530

    UI | Security : user cannot navigate to any application pages without login.
    Change-Id: I6fd56801a0858f7e0107ebdb6ccf9e4ae9fae302
    Signed-off-by: Karnan <kchidamb@redhat.com>

--- app/components/base/application-controller.ts
+++ app/components/base/application-controller.ts
@@ -1,8 +1,16 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
+import {UserService} from '../rest/user';
+
 export class ApplicationController {
-    static $inject: Array<string> = ['$scope'];
-    constructor($scope: any) {
+    static $inject: Array<string> = ['$scope', '$location', 'UserService'];
+    constructor($scope: any, locationService: ng.ILocationService, userService: UserService) {
         $scope.expandedView = true;
+
+        $scope.$on( "$routeChangeStart", function(event, next, current) {
+                userService.getCurrentUser().catch(() => {
+                    locationService.path('/');
+                });
+        });
     }
 }
