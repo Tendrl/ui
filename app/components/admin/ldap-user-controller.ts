4fcff956c993f4edf999fdf9eaf5fda5dcd3b2e7
Karnan <kchidamb@redhat.com>
2016-02-12 12:14:40 +0530

    import ldap users to skyring
    added import buttons in ldap users table
    to import users to skyring.
    
    Change-Id: I5971b027c774af1507f06e200207785cff37fbfa
    Signed-off-by: Karnan <kchidamb@redhat.com>

--- app/components/admin/ldap-user-controller.ts
+++ app/components/admin/ldap-user-controller.ts
@@ -3,6 +3,7 @@ import {UserService} from '../rest/user';
 export class LdapUserController {
     private errorMsg;
     private userList : Array<any>;
+    private allUserList : Array<any>;
     static $inject: Array<string> = [
         '$location',
         'UserService',
@@ -11,12 +12,32 @@ export class LdapUserController {
     constructor(
         private $location: ng.ILocationService,
         private UserService: UserService) {
+        this.getUsers();
         this.getLdapUsers();
     }
-   
+
     public getLdapUsers(){
         this.UserService.getLdapUsers().then((users)=>{
             this.userList = users;
+            var diff = _.difference(_.pluck(this.userList, "username"), _.pluck(this.allUserList, "username"));
+            this.userList = _.filter(this.userList, function(obj) {
+                var index = diff.indexOf(obj.username);
+                if(index >= 0 ){
+                    obj.imported = false;
+                }
+                   return true;
+                 });
+        });
+    }
+
+   public getUsers(){
+        this.UserService.getUsers().then((users)=>{
+            this.allUserList = users;
+        });
+    }
+
+    public addLdapUser(user){
+        this.UserService.addUser(user).then((user)=>{
         });
     }
 
