kmkanagaraj/usm-client-2

Enables Ldap configuration through UI
users can configure the ldap connection settings
through Ldap config page

Change-Id: If7ba71687b0a313a8b3974bfdfa0ae30a1b50a3e
Signed-off-by: Karnan <kchidamb@redhat.com>

app/components/admin/ldap-config-controller.ts
@@ -1,0 +1,53 @@
+import {LdapService} from '../rest/ldap';
+
+export class LdapConfigController {
+    private errorMsg: boolean;
+    private ldapserver : string;
+    private port: number;
+    private base: string;
+    private domainadmin: string;
+    private password:string;
+    
+    static $inject: Array<string> = [
+        '$location',
+        'LdapService',
+    ];
+
+    constructor(
+        private $location: ng.ILocationService,
+        private LdapService: LdapService) {
+            this.getConfig();
+    }
+
+    public save():void {
+        var config = {
+            ldapserver: this.ldapserver,
+            port: this.port,
+            base: this.base,
+            domainadmin: this.domainadmin,
+            password: this.password,
+            uid: "cn",
+            firstname: "cn",
+            lastname: "sn",
+            mail: "mail"
+        };
+
+        this.LdapService.saveLdapConfig(config).then((result) => {
+            if(result.status === 200) {
+                this.errorMsg = false;
+            }else{
+                this.errorMsg = true;
+            }
+        });
+     }
+     
+     public getConfig(){
+         this.LdapService.getLdapConfig().then((config)=>{
+            this.ldapserver = config.ldapserver,
+            this.port = config.port,
+            this.base =  config.base,
+            this.domainadmin = config.domainadmin
+        });
+     }
+
+}

app/components/rest/ldap.ts
@@ -1,0 +1,26 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+export class LdapService {
+    rest: restangular.IService;
+    restFull: restangular.IService;
+    static $inject: Array<string> = ['Restangular'];
+    constructor(rest: restangular.ICollection) {
+        this.rest = rest.withConfig((RestangularConfigurer) => {
+            RestangularConfigurer.setBaseUrl('/api/v1/');
+        });
+        this.restFull = rest.withConfig((RestangularConfigurer) => {
+            RestangularConfigurer.setBaseUrl('/api/v1/');
+            RestangularConfigurer.setFullResponse(true);
+        });
+    }
+
+    saveLdapConfig(config){
+        return this.restFull.all('users').post(config);
+    }
+
+    getLdapConfig() {
+        return this.rest.one('ldap').get().then(function(config) {
+            return config;
+        });
+    }
+}

Tendrl/ui

app/components/admin/ldap-config-controller.ts
@@ -1,0 +1,54 @@
+import {LdapService} from '../rest/ldap';
+
+export class LdapConfigController {
+    private errorMsg: boolean;
+    private ldapServer : string;
+    private port: string;
+    private base: string;
+    private domainAdmin: string;
+    private password:string;
+
+    static $inject: Array<string> = [
+        '$location',
+        'LdapService',
+    ];
+
+    constructor(
+        private $location: ng.ILocationService,
+        private LdapService: LdapService) {
+            this.getConfig();
+    }
+
+    public save():void {
+        var config = {
+            ldapserver: this.ldapServer,
+            port: parseInt(this.port),
+            base: this.base,
+            domainadmin: this.domainAdmin,
+            password: this.password,
+            uid: "cn",
+            firstname: "displayName",
+            lastname: "sn",
+            displayname: "",
+            email: "mail"
+        };
+
+        this.LdapService.saveLdapConfig(config).then((result) => {
+            if(result.status === 200) {
+                this.errorMsg = false;
+            }else{
+                this.errorMsg = true;
+            }
+        });
+     }
+
+     public getConfig(){
+         this.LdapService.getLdapConfig().then((config)=>{
+            this.ldapServer = config.ldapserver,
+            this.port = config.port,
+            this.base =  config.base,
+            this.domainAdmin = config.domainadmin
+        });
+     }
+
+}

app/components/rest/ldap.ts
@@ -1,0 +1,26 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+export class LdapService {
+    rest: restangular.IService;
+    restFull: restangular.IService;
+    static $inject: Array<string> = ['Restangular'];
+    constructor(rest: restangular.ICollection) {
+        this.rest = rest.withConfig((RestangularConfigurer) => {
+            RestangularConfigurer.setBaseUrl('/api/v1/');
+        });
+        this.restFull = rest.withConfig((RestangularConfigurer) => {
+            RestangularConfigurer.setBaseUrl('/api/v1/');
+            RestangularConfigurer.setFullResponse(true);
+        });
+    }
+
+    saveLdapConfig(config){
+        return this.restFull.all('ldap').post(config);
+    }
+
+    getLdapConfig() {
+        return this.rest.one('ldap').get().then(function(config) {
+            return config;
+        });
+    }
+}