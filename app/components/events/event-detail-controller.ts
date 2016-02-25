kmkanagaraj/usm-client-2

Refractor events and tasks
Moved existing tasks pages under /tasks and introduced events.

Change-Id: I27cc0f0e5511dbaf13151da4642b4ff902de29e0
Signed-off-by: Karnan <kchidamb@redhat.com>

app/components/events/event-detail-controller.ts
@@ -1,0 +1,37 @@
+// <reference path="../../../typings/tsd.d.ts" />
+
+import {RequestService} from '../rest/request';
+
+export class EventDetailController {
+    private list: Array<any>;
+    private timer;
+    private eventId: string;
+    private detail: any;
+    static $inject: Array<string> = [
+        '$scope',
+        '$interval',
+        '$routeParams',
+        'RequestService'
+    ];
+    constructor(
+        private $scope: ng.IScope,
+        private $interval: ng.IIntervalService,
+        private routeParamsSvc: ng.route.IRouteParamsService,
+        private requestSvc: RequestService) {
+        this.eventId = this.routeParamsSvc['eventId'];
+        this.timer = this.$interval(() => this.refresh(), 5000);
+        this.$scope.$on('$destroy', () => {
+            this.$interval.cancel(this.timer);
+        });
+        this.refresh();
+    }
+
+    public refresh() {
+        this.requestSvc.get(this.eventId).then(task => {
+            this.detail = task;
+            if(task.completed == true){
+                this.$interval.cancel(this.timer);
+            }
+        });
+    }
+}

app/components/rest/events.ts
@@ -1,41 +1,59 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
 export class EventService {
+    // **Constant variable** constant variable for *pageSize*.
+    pageSize = 32;
     rest: restangular.IService;
-    restFull: restangular.IService;
     static $inject: Array<string> = ['Restangular'];
     constructor(rest: restangular.ICollection) {
         this.rest = rest.withConfig((RestangularConfigurer) => {
             RestangularConfigurer.setBaseUrl('/api/v1/');
         });
-        this.restFull = rest.withConfig((RestangularConfigurer) => {
-            RestangularConfigurer.setBaseUrl('/api/v1/');
-            RestangularConfigurer.setFullResponse(true);
-        });
     }
-
 
     // **getList**
-    // **@returns** a promise with all events.
-    getList() {
-        return this.rest.all('events').getList().then(function(events) {
-            return events;
-        });
-    }
-
-    // **getListByCluster**
-    // **@returns** a promise  with all events of the cluster.
-    getListByCluster(clusterId) {
-        return this.rest.one('clusters', clusterId).all('events').getList().then(function(events) {
-            return events;
+    // **@returns** a promise with list of tasks.
+    getList(pageNumber,pageSize) {
+        return this.rest.one('tasks').get({
+            pageNo: pageNumber,
+            pageSize: pageSize
+        }).then(function(tasks) {
+            return tasks;
         });
     }
 
     // **get**
-    // **@returns** a promise with event detaisl.
-    get(eventId) {
-        return this.rest.one('events', eventId).get().then(function(event) {
-            return event;
+    // **@returns** a promise with the task.
+    get(id) {
+        return this.rest.one('tasks', id).get().then(function(task) {
+            return task;
+        });
+    }
+
+    // **getComplete**
+    // **@returns** a promise with most recent *pageSize* completed requests.
+    getComplete() {
+        /* jshint camelcase: false */
+        return this.rest.customGETLIST('tasks', {
+            state: 'complete',
+            page_size: this.pageSize
+        });
+    }
+
+    // **getSubmitted**
+    // **@returns** a promise with most recent *pageSize* submitted requests.
+    getSubmitted() {
+        /* jshint camelcase: false */
+        return this.rest.customGETLIST('tasks', {
+            state: 'submitted',
+            page_size: this.pageSize
+        });
+    }
+    // **getSubTasks**
+    // **@returns** a promise with list of subtasks.
+    getSubTasks(id) {
+        return this.rest.one('tasks', id).getList('subtasks').then(function(task) {
+            return task;
         });
     }
 }

Tendrl/ui

app/components/events/event-detail-controller.ts
@@ -1,0 +1,36 @@
+// <reference path="../../../typings/tsd.d.ts" />
+
+import {RequestService} from '../rest/request';
+
+export class EventDetailController {
+    private timer;
+    private eventId: string;
+    private detail: any;
+    static $inject: Array<string> = [
+        '$scope',
+        '$interval',
+        '$routeParams',
+        'RequestService'
+    ];
+    constructor(
+        private $scope: ng.IScope,
+        private $interval: ng.IIntervalService,
+        private routeParamsSvc: ng.route.IRouteParamsService,
+        private requestSvc: RequestService) {
+        this.eventId = this.routeParamsSvc['eventId'];
+        this.timer = this.$interval(() => this.refresh(), 5000);
+        this.$scope.$on('$destroy', () => {
+            this.$interval.cancel(this.timer);
+        });
+        this.refresh();
+    }
+
+    public refresh() {
+        this.requestSvc.get(this.eventId).then(event => {
+            this.detail = event;
+            if(event.completed == true){
+                this.$interval.cancel(this.timer);
+            }
+        });
+    }
+}

app/components/rest/events.ts
@@ -2,40 +2,29 @@
 
 export class EventService {
     rest: restangular.IService;
-    restFull: restangular.IService;
     static $inject: Array<string> = ['Restangular'];
     constructor(rest: restangular.ICollection) {
         this.rest = rest.withConfig((RestangularConfigurer) => {
             RestangularConfigurer.setBaseUrl('/api/v1/');
         });
-        this.restFull = rest.withConfig((RestangularConfigurer) => {
-            RestangularConfigurer.setBaseUrl('/api/v1/');
-            RestangularConfigurer.setFullResponse(true);
-        });
     }
-
 
     // **getList**
-    // **@returns** a promise with all events.
-    getList() {
-        return this.rest.all('events').getList().then(function(events) {
-            return events;
-        });
-    }
-
-    // **getListByCluster**
-    // **@returns** a promise  with all events of the cluster.
-    getListByCluster(clusterId) {
-        return this.rest.one('clusters', clusterId).all('events').getList().then(function(events) {
-            return events;
+    // **@returns** a promise with list of tasks.
+    getList(pageNumber,pageSize) {
+        return this.rest.one('events').get({
+            pageNo: pageNumber,
+            pageSize: pageSize
+        }).then(function(tasks) {
+            return tasks;
         });
     }
 
     // **get**
-    // **@returns** a promise with event detaisl.
-    get(eventId) {
-        return this.rest.one('events', eventId).get().then(function(event) {
-            return event;
+    // **@returns** a promise with the task.
+    get(id) {
+        return this.rest.one('events', id).get().then(function(task) {
+            return task;
         });
     }
 }