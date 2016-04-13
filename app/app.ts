kmkanagaraj/usm-client-2

cluster dashboard - added configuration page
added configuration details in cluster level

Change-Id: Icaabc8245112c6c2109a2bc788898926269576be
Signed-off-by: kamleshKumarVerma <kaverma@redhat.com>

app/app.ts
@@ -35,6 +35,7 @@ import {StorageSizeSelector} from "./components/shared/directives/storage-size-s
 import {OsdDetail} from "./components/clusters/osdsdetail/osd-detail-directive";
 import {HostList} from "./components/clusters/hostdetail/host-list-directive";
 import {StorageProfileDisks} from './components/clusters/storageprofile/storageprofile-disks';
+import {ConfigDetail} from "./components/clusters/configdetail/config-detail-directive";
 
 import {BytesFilter} from './components/shared/filters/bytes';
 
@@ -101,6 +102,7 @@ class USMApp {
             .directive('osdDetail', () => new OsdDetail())
             .directive('hostList', () => new HostList())
             .directive('storageprofileDisks', () => new StorageProfileDisks())
+            .directive('configDetail', () => new ConfigDetail())
             .filter('bytes', BytesFilter)
             .controller('LdapConfigController',LdapConfigController)
             .controller('EventListController',EventListController)

app/components/rest/clusters.ts
@@ -215,4 +215,12 @@ export class ClusterService {
         });
     }
 
+    // **getClusterConfig**
+    // **@returns** a promise with cluster configuration details.
+    getClusterConfig(clusterId) {
+        return this.rest.one('clusters/'+clusterId+'/config').getList().then(function(cluster_config) {
+            return cluster_config;
+        });
+    }
+
 }

Tendrl/ui

app/app.ts
@@ -34,6 +34,7 @@ import {StorageSizeSelector} from "./components/shared/directives/storage-size-s
 import {OsdDetail} from "./components/clusters/osdsdetail/osd-detail-directive";
 import {HostList} from "./components/clusters/hostdetail/host-list-directive";
 import {StorageProfileDisks} from './components/clusters/storageprofile/storageprofile-disks';
+import {ClusterConfigDetail} from "./components/clusters/configdetail/config-detail-directive";
 
 import {BytesFilter} from './components/shared/filters/bytes';
 
@@ -101,6 +102,7 @@ class USMApp {
             .directive('osdDetail', () => new OsdDetail())
             .directive('hostList', () => new HostList())
             .directive('storageprofileDisks', () => new StorageProfileDisks())
+            .directive('clusterConfigDetail', () => new ClusterConfigDetail())
             .filter('bytes', BytesFilter)
             .controller('LdapConfigController',LdapConfigController)
             .controller('TaskListController',TaskListController)

app/components/rest/clusters.ts
@@ -215,4 +215,12 @@ export class ClusterService {
         });
     }
 
+    // **getClusterConfig**
+    // **@returns** a promise with cluster configuration details.
+    getClusterConfig(clusterId) {
+        return this.rest.one('clusters/'+clusterId+'/config').get().then(function(cluster_config) {
+            return cluster_config;
+        });
+    }
+
 }