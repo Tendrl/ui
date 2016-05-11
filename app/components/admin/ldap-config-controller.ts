cd9983eece37c6cea51f1ff22920040d625729b4
Timothy Asir Jeyasingh <tjeyasin@redhat.com>
2016-05-11 16:30:36 +0530

    ldap: add ui to select directory service type
    Change-Id: Idab8eb28d867179aed5a17da43faddd7dfbba237
    Signed-off-by: Timothy Asir Jeyasingh <tjeyasin@redhat.com>
    Signed-off-by: Timothy Asir J <tjeyasin@redhat.com>

--- app/components/admin/ldap-config-controller.ts
+++ app/components/admin/ldap-config-controller.ts
@@ -2,6 +2,7 @@ import {LdapService} from '../rest/ldap';
 
 export class LdapConfigController {
     private errorMsg: boolean;
+    private ldapType: string;
     private ldapServer : string;
     private port: string;
     private base: string;
@@ -21,16 +22,12 @@ export class LdapConfigController {
 
     public save():void {
         var config = {
+            ldaptype: this.ldapType,
             ldapserver: this.ldapServer,
             port: parseInt(this.port),
             base: this.base,
             domainadmin: this.domainAdmin,
-            password: this.password,
-            uid: "cn",
-            firstname: "displayName",
-            lastname: "sn",
-            displayname: "",
-            email: "mail"
+            password: this.password
         };
 
         this.LdapService.saveLdapConfig(config).then((result) => {
@@ -44,6 +41,7 @@ export class LdapConfigController {
 
      public getConfig(){
          this.LdapService.getLdapConfig().then((config)=>{
+            this.ldapType = config.ldaptype,
             this.ldapServer = config.ldapserver,
             this.port = config.port,
             this.base =  config.base,
