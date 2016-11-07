03d3d9afb2e040a80e3748c0192c85730f9617bd
Neha Gupta <gnehapk@gmail.com>
2016-11-07 19:36:41 +0530

    Updated folder structure and testing framework added - Gulpfile to be updated - package.json to be updated - Leaves old repo code as is in /app - New code added under /src
    Signed-off-by: Neha Gupta <gnehapk@gmail.com>

---
+++ karma.conf.js
@@ -0,0 +1,59 @@
+module.exports = function (config) {
+
+    config.set({
+        basePath: "",
+        frameworks: ["mocha", "chai", "sinon"],
+        files: [
+
+            "dist/Tendrl/jsLibraries/libraries.js",
+            "node_modules/angular-mocks/angular-mocks.js",
+
+            // Add template files
+            "src/**/*.html",
+
+            "src/commons/js/*.js",
+            "src/modules/**/*.js",
+
+            // Add all the test files
+            "test/unit/*.js",
+        ],
+
+        exclude: [],
+
+        preprocessors: {
+            "/**/*.html": "ng-html2js",
+            "src/**/*.js": "coverage"
+        },
+
+        ngHtml2JsPreprocessor: {
+            stripPrefix: "src/",
+            moduleName: "templates"
+        },
+
+        //reporters: ["progress", "coverage"],
+        reporters: ["mocha", "coverage"],
+        mochaReporter: {
+            // full, autowatch, minimal
+            output: "autowatch",
+            ignoreSkipped: false
+        },
+
+        browserNoActivityTimeout: 20000,
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ["PhantomJS"],
+        singleRun: true,
+        coverageReporter: {
+            dir: "./coverage",
+            reporters: [{
+                type: "cobertura",
+                subdir: ".",
+                file: "cobertura.xml"
+            }, {
+                type: "text"
+            }]
+        }
+    });
+};
