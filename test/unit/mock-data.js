bf9e45f2836b365afe20683225738520f34025fd
Soumya Deb <debloper@gmail.com>
2016-12-01 18:22:29 +0530

    Test coverage for node and cluster listing
    - Mock data taken from live server
    - Fixes Jira ticket TEN-152 and TEN-153
    
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- test/unit/mock-data.js
+++ test/unit/mock-data.js
@@ -216,4 +216,59 @@
         }
     });
 
+    testDataModule.value("nodeList", {
+        list: [
+            {
+                "fqdn":"dhcp41-176.domain.com",
+                "machine_id":"856b3135989b47a99bd99100c0d0d87e",
+                "node_id":"c6b9df01-1c73-42f9-93bc-0ccc21005984"
+            },
+            {
+                "fqdn":"dhcp43-2.domain.com",
+                "machine_id":"3ebd5baa44014e4a9b74cada7cdf712d",
+                "node_id":"5cb04cdd-d2d8-4a92-965d-27af9b648ce5"
+            },
+            {
+                "fqdn":"dhcp43-189.domain.com",
+                "machine_id":"a2e4163a1a8e4b28afb1a17e6a1d9962",
+                "node_id":"3038c577-b233-4513-926d-a8a1ac63b0a3"
+            },
+            {
+                "fqdn":"dhcp42-98.domain.com",
+                "machine_id":"3a56e73867414b4bb2fb08173c66eea9",
+                "node_id":"7a880068-bf5e-465f-a083-0c23aeaf30f4"
+            },
+            {
+                "fqdn":"dhcp41-229.domain.com",
+                "machine_id":"a4d53abc5f3b47778b5d349c814bc609",
+                "node_id":"027db80d-eee9-40b6-8e59-8c81b80aa655"
+            },
+            {
+                "fqdn":"dhcp41-178.domain.com",
+                "machine_id":"0c0edb7a7b594db9b7d3b051d2a4d1bd",
+                "node_id":"9847aeef-a47c-4737-8a1e-059b9ebeafe9"
+            }
+        ]
+    });
+
+    testDataModule.value("clusterList", {
+        list: [
+            {
+                "sds_name":"gluster",
+                "sds_version":"3.9rc1",
+                "cluster_id":"49f878e3-e253-4fad-a947-cb987bba503d"
+            },
+            {
+                "sds_name":"ceph",
+                "sds_version":"10.2.3",
+                "cluster_id":"8b0c637d-f5f7-40f6-acd9-50d5feb5f4b3"
+            },
+            {
+                "sds_name":"ceph",
+                "sds_version":"10.2.3",
+                "cluster_id":"8bec7519-f781-47f7-86eb-657e7e194b59"
+            }
+        ]
+    });
+
 })();
---
+++ test/unit/node-list-test.js
@@ -0,0 +1,62 @@
+describe("Unit: List Nodes", function () {
+    "use strict";
+
+    // Angular injectables
+    var $q, $httpBackend, $injector, $controller, $rootScope, $state, $location, $templateCache, $compile;
+
+    // Module defined (non-Angular) injectables
+    var $scope, config, utils, nodeList;
+
+    // Local variables used for testing
+    var getListDeferred, vm;
+
+    // Initialize modules
+    beforeEach(function () {
+        module("TendrlModule");
+        module("TestDataModule");
+        module("templates");
+    });
+
+    beforeEach(function () {
+
+        var templateHtml;
+
+        inject(function (_$q_, _$controller_, _$rootScope_, _$state_, _$templateCache_, _$compile_, _nodeList_, _$location_) {
+            $q = _$q_;
+            $controller = _$controller_;
+            $rootScope = _$rootScope_;
+            $location = _$location_;
+            $state = _$state_;
+            $templateCache = _$templateCache_;
+            $compile = _$compile_;
+            nodeList = _nodeList_;
+
+            $scope = $rootScope.$new();
+            templateHtml = $templateCache.get("/modules/node/node.html");
+
+            $compile(templateHtml)($scope);
+        });
+
+        inject(function (_utils_) {
+            utils = _utils_;
+        });
+    });
+
+    beforeEach(function () {
+
+        getListDeferred = $q.defer();
+        sinon.stub(utils, "getObjectList").returns(getListDeferred.promise);
+
+        $state.current.name = "node";
+
+        vm = $controller("nodeController", { $scope: $scope });
+
+    });
+
+    it("Should get list of nodes", function() {
+        getListDeferred.resolve(nodeList.list);
+        $scope.$digest();
+        expect(vm.nodeList).to.deep.equal(nodeList.list);
+    });
+
+});
