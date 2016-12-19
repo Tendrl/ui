af3cd92d86cf32f2be946d16e8683e6814e4fac1
kamleshKumarVerma <kaverma@redhat.com>
2016-12-19 12:37:49 +0530

    donut chart implementation
    ============= How to use "donutChart" directive ======
    
        - chartTitle (optional):  expected string for title.
            NOTE:-  In UX design some of the place "title" is not there. so it should be optional.
    
        - chartData: Should be object with three properties - total, used and percent-used
            NOTE: - Here "total" and "used" are optional , But "percent-used" is mandatory property.
            Example: -
    
            (In your controller):
            vm.chartData = {"used":352460800,"total":3524608000, "percent-used": 10} (defined chartData object)
    
            (In your template):
            <div ng-repeat="host in hostList"
                <div  class="col-md-2">
                    <div class="list-view-pf-additional-info-item donut-chart" chart-title="Storage" chart-data="node.chartData">
                    </div>
                </div>
            </div>
    
            NOTE: if "vm.chartData.percent-used" is not defined . than automatically "donut chart" will show "no data available".
    
        - chartThresholds (optional) : if user will not pass thresholds value than it will have default value:
            {'warning':'60','error':'90'} (which is defined in "config.json"- because it should be configurable)
    
            (In your controller):
            vm.chartThresholds = {'warning':'40','error':'90'}; (defined chartThresholds object)
    
            (In your template):
             <div ng-repeat="host in hostList"
                <div  class="col-md-2">
                    <div class="list-view-pf-additional-info-item donut-chart" chart-title="Storage" chart-data="node.chartData" chart-thresholds="chartThresholds">
                    </div>
                </div>
            </div>
    
    Signed-off-by: kamleshKumarVerma <kaverma@redhat.com>

---
+++ src/commons/filters/bytes.js
@@ -0,0 +1,22 @@
+/*
+ * This can be used like following
+ *
+ * {{ someexpresssion | bytes }}
+*/
+
+(function () {
+    "use strict";
+    var app = angular.module("TendrlModule");
+    
+    app.filter('bytes', function() {
+        return function(bytes, precision) {
+            if(bytes === 0) return "0.0 bytes"
+            if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+            if (typeof precision === 'undefined') precision = 1;
+            var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+                number = Math.floor(Math.log(bytes) / Math.log(1024));
+            return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+        }
+    });
+
+}());
