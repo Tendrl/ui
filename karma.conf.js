e818e36061361de7fe7c64d967d19136d3fd2954
Neha Gupta <gnehapk@gmail.com>
2016-12-23 11:30:42 +0530

    Modified cluster list view as per the new json format

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,10 @@ module.exports = function (config) {
             "src/commons/components/**/*.js",
 
             // Add all the test files
-            "test/unit/*.js"
+            "test/unit/basic-test.js",
+            "test/unit/generate-form-directive-test.js",
+            "test/unit/generate-form--field-directive-test.js",
+            "test/unit/mock-data.js"
         ],
 
         exclude: [],
