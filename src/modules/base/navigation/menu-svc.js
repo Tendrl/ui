64ffc327f29ec5a73e0aadc33162633a0b60b0c5
Neha Gupta <gnehapk@gmail.com>
2017-07-26 16:33:59 +0530

    Removed all modules which are not needed

--- src/modules/base/navigation/menu-svc.js
+++ src/modules/base/navigation/menu-svc.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     "use strict";
 
     var app = angular.module("TendrlModule");
@@ -11,14 +11,6 @@
         /* Cache the reference to this pointer */
         var vm = this;
         vm.menus = [{
-            label: "Dashboard",
-            id: "dashboard",
-            href: "dashboard",
-            icon: "pficon pficon-resource-pool",
-            active: false,
-            hasSubMenus: false
-        },
-        {
             label: "Clusters",
             id: "cluster",
             href: "cluster",
@@ -30,27 +22,6 @@
             id: "host",
             href: "host",
             icon: "pficon pficon-container-node",
-            active: false,
-            hasSubMenus: false
-        }, {
-            label: "Volumes",
-            id: "file-share",
-            href: "file-share",
-            icon: "pficon pficon-volume",
-            active: false,
-            hasSubMenus: false
-        }, {
-            label: "Pools",
-            id: "pool",
-            href: "pool",
-            icon: "pficon pficon-resource-pool",
-            active: false,
-            hasSubMenus: false
-        }, {
-            label: "RBDs",
-            id: "rbd",
-            href: "rbd",
-            icon: "pficon pficon-resource-pool",
             active: false,
             hasSubMenus: false
         }, {
@@ -78,24 +49,24 @@
 
         vm.setActive = function(menuId) {
 
-            if(JSON.parse(localStorage.getItem("userInfo"))) {
-                vm.menus.map(function(menu){
-                    if (menu.hasSubMenus===true){
-                        menu.subMenus.map(function(submenu){
+            if (JSON.parse(localStorage.getItem("userInfo"))) {
+                vm.menus.map(function(menu) {
+                    if (menu.hasSubMenus === true) {
+                        menu.subMenus.map(function(submenu) {
                             submenu.active = submenu.id === menuId
                         });
                     }
                     menu.active = menu.id === menuId;
                     return menu;
                 });
-            } else if($http.defaults.headers.common["Authorization"]){
+            } else if ($http.defaults.headers.common["Authorization"]) {
                 AuthManager.logout();
                 $state.go("login");
-                AuthManager.setFlags(); 
+                AuthManager.setFlags();
                 AuthManager.isUserLoggedIn = false;
             }
         };
-        
+
         vm.getMenus = function() {
             return vm.menus;
         };
